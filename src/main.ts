eca5713e7f318bd7ad582f999487eeea59b2d7e9
Koushik Dutta <koushd@gmail.com>
2019-03-15 00:18:39 -0700

    update

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,39 @@
 declare var log: any;
 
 // need to polyfill Promises
-import "core-js/modules/es6.promise";
+// import "core-js/modules/es6.promise";
 declare var Promise: any;
 
 import axios from 'axios';
+import { OnOff } from '@scrypted/sdk';
 
-log.i('Hello World');
+log.i('Hello World. This will create a virtual OnOff device.');
 
-class Program {
-    public static wait(ms): Promise<void> {
-        return new Promise((resolve, reject) => {
-            setTimeout(resolve, ms);
-        })
+class Device implements OnOff {
+    _isOn: boolean;
+
+    constructor() {
+        this._isOn = false;
     }
+    isOn(): boolean {
+        return this._isOn;
+    }
+    turnOff(): void {
+        this._isOn = false;
+        log.i('turnOff was called!');
+    }
+    async turnOn(): Promise<void> {
+        // set a breakpoint here.
+        log.i('turnOn was called!');
 
-    public static async main(): Promise<void> {
-        log.i('setTimeout is implemented by the Android host.');
-        await Program.wait(50);
-        log.i('done waiting for 50ms.');
-        log.i('XMLHttpRequest is polyfilled by the Android host. This allows the popular http library axios or jquery ajax to work.');
-
+        // turnOn must return immediately, but it can trigger other things... 
+        log.i('XMLHttpRequest is polyfilled by the Android host.');
+        log.i('This allows the popular http library axios to work.');
         const ip = await axios.get('http://jsonip.com');
         log.i(`my ip: ${ip.data.ip}`);
+
+        this._isOn = true;
     }
 }
 
-log.i('The script will exit, but the async function will continue.');
-
-Program.main();
+export default new Device();
