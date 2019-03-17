4bbc6585d1428101b44c563ab9312703e160f47d
Koushik Dutta <koushd@gmail.com>
2019-03-17 14:05:20 -0700

    fixup

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
-// log is a local script variable provided by Scrypted.
+// https://developer.scrypted.app/#getting-started
 import axios from 'axios';
 import { OnOff } from '@scrypted/sdk';
 import sdk from '@scrypted/sdk';
-const { log, scriptSettings } = sdk;
-
-scriptSettings.putString("foo", "bar");
+const { log } = sdk;
 
 log.i('Hello World. This will create a virtual OnOff device.');
 
 class Device implements OnOff {
     _isOn: boolean;
-
     constructor() {
         this._isOn = false;
     }
@@ -18,16 +15,14 @@ class Device implements OnOff {
         return this._isOn;
     }
     turnOff(): void {
-        this._isOn = false;
         log.i('turnOff was called!');
+        this._isOn = false;
     }
     async turnOn(): Promise<void> {
         // set a breakpoint here.
         log.i('turnOn was called!');
 
-        // turnOn must return immediately, but it can trigger other things... 
-        log.i('XMLHttpRequest is polyfilled by the Android host.');
-        log.i('This allows the popular http library axios to work.');
+        log.i("Let's pretend to perform a web request on an API that would turn on a light.");
         const ip = await axios.get('http://jsonip.com');
         log.i(`my ip: ${ip.data.ip}`);
 
